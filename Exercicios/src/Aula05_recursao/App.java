191fe6c5f9b1d7bfa2bc3b8455a7604df49409af
Andreas Berwaldt <68302791+andreaspsb@users.noreply.github.com>
2025-03-18 00:56:31 +0000

    acrescimos

--- Exercicios/src/Aula05_recursao/App.java
+++ Exercicios/src/Aula05_recursao/App.java
@@ -1,33 +1,44 @@
 package Aula05_recursao;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println(Exercicio02(6));
+        System.out.println(exercicio02(6));
+
+        int[] array = new int[4];
+        array[0] = 2;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 2;
+        System.out.println(Arrays.toString(exercicio04NaoRecursivo(array)));
+        System.out.println(exercicio05NaoRecursivo(array));
     }
 
-    public static void Exercicio01(int n) {
+    public static void exercicio01(int n) {
         if (n > 0) {
             System.out.println("oi");
-            Exercicio01(n - 1);
+            exercicio01(n - 1);
         }
     }
 
-    public static int Exercicio02(int n) {
+    public static int exercicio02(int n) {
         int retorno = 0;
         if (n > 0) {
-            retorno = n + Exercicio02(n - 1);
+            retorno = n + exercicio02(n - 1);
         }
         return retorno;
     }
 
     public static int fatorialRecursivo(int n) {
-        if (n < 0) return -1;
+        if (n < 0)
+            return -1;
         if (n == 0 || n == 1)
             return 1;
         return n * fatorialRecursivo(n - 1);
     }
 
-    public static int Exercicio03(int n) {
+    public static int exercicio03(int n) {
         int f = 1;
         for (int i = n; i < 0; i--) {
             f = f * i;
@@ -35,17 +46,55 @@ public class App {
         return f;
     }
 
-    //Implemente um método recursivo em Java que
-    //inverta um array de inteiros de tamanho n.
-    public static int[] Exercicio04(int[] array) {
+    // Implemente um método recursivo em Java que
+    // inverta um array de inteiros de tamanho n.
+    public static int[] exercicio04NaoRecursivo(int[] array) {
+        int[] retorno = new int[array.length];
+        int n = 0;
 
+        for (int i = array.length - 1; i > -1; i--) {
+            retorno[n] = array[i];
+            n++;
+        }
+
+        return retorno;
+    }
+
+    // Implemente um método recursivo em Java que
+    // inverta um array de inteiros de tamanho n.
+    public static int[] exercicio04(int[] array, int aux1, int aux2) {
+
+        retorno[aux1] = array[aux2];
+        exercicio04(array, aux1, aux2);
+        return null;
     }
 
     // um método recursivo que receba um
-    //array de inteiros e devolva o maior valor dentro
+    // array de inteiros e devolva o maior valor dentro
     // desse array.
-    public static int Exercicio05(int[] array) {
+    public static int exercicio05NaoRecursivo(int[] array) {
+        int maiorValor = array[0];
 
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maiorValor) {
+                maiorValor = array[i];
+            }
+        }
+
+        return maiorValor;
+    }
+
+    // um método recursivo que receba um
+    // array de inteiros e devolva o maior valor dentro
+    // desse array.
+    public static int exercicio05(int[] array, int aux) {
+        int maiorValor = array[0];
+
+        if (array[n] > maiorValor) {
+            
+        }
+
+        return maiorValor;
     }
 
 }
